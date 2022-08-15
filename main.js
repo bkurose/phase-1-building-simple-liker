fec56ca5d874a76120617db319916f3f90b66e63
Ben Kurose <ben.kurose@gmail.com>
2022-08-15 14:51:46 -0400

    commit tree 2aafd8c3bbcbf3ae78d33125484e659d680332ea

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', createLikerButton)
 
+const errorMessage = document.querySelector('#modal')
 
+// function makesHiddenStart(){
+//   return errorMessage.hidden = true;
+// }
+// makesHiddenStart()
 
+function createLikerButton(){
+  let likerButton = document.querySelectorAll('.like-glyph');
+  likerButton.forEach(button => clicker(button));
+}
+
+function clicker(button){
+  button.addEventListener('click', updateLikeStatus);
+}
+
+function updateLikeStatus(e){
+  mimicServerCall()
+  .then(() => {
+  e.target.innerHTML = FULL_HEART
+  e.target.classList.toggle('activated-heart')
+  })
+  .catch(() => {
+    errorMessage.toggle('hidden')
+    setTimeout(() => errorMessage.toggle('hidden'), 3000)
+  });
+}
+
+function logger() {console.log('whatever')}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
